942f5588278e62c2b68c18774fa04c6d66f89934
shivam anand <shivam850anand@gmail.com>
2024-12-28 22:28:17 +0530

    legal management tool

---
+++ client/src/app/(home)/_components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="absolute inset-0 w-full bg-primary p-2 h-min">
+      AppHeader
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Header from "./_components/header";
+
+export default function HomePage() {
+  return (
+    <main className="relative min-h-screen">
+      <Header />
+    </main>
+  );
+}
